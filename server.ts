989b01ab8b193cadf9a7f30dc79aa409b401e771
joshpadernal29 <josh.padernal29@gmail.com>
2026-04-17 00:41:57 +0800

    added imports form the files inside the server.ts

--- server.ts
+++ server.ts
@@ -1,11 +1,29 @@
-const express = require('express');
-const swaggerRouter = require('./swagger');
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import errorHandler from './_middleware/error-handler';
+import accountsController from './accounts/accounts.controller';
+import swaggerDocs from './_helpers/swagger';
 
 const app = express();
 
-app.use('/api-docs', swaggerRouter);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.listen(4000, () => {
-  console.log('Server running on http://localhost:4000');
-  console.log('Swagger UI at http://localhost:4000/api-docs');
-});
+// allow cors requests from any origin and with credentials
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+
+// api routes
+app.use('/accounts', accountsController);
+
+// swagger docs route
+app.use('/api-docs', swaggerDocs);
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => console.log('Server listening on port ' + port));
