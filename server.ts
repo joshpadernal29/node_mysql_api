4e195e0b154489830354df8a7a1a56f5ce1b8997
joshpadernal29 <josh.padernal29@gmail.com>
2026-04-19 17:42:45 +0800

    added console log for swagger

--- server.ts
+++ server.ts
@@ -2,13 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-
-// uncomment after finishing all files
-// import errorHandler from './_middleware/error-handler';
-// import accountsController from './accounts/accounts.controller';
-
-
-import swaggerDocs from './_helpers/swagger.js';
+import errorHandler from './_middleware/error-handler';
+import accountsController from './accounts/accounts.controller';
+import swaggerDocs from './_helpers/swagger';
 
 const app = express();
 
@@ -19,15 +15,16 @@ app.use(cookieParser());
 // allow cors requests from any origin and with credentials
 app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
 
-// app.use('/accounts', accountsController);
+// api routes
+app.use('/accounts', accountsController);
 
+// swagger docs route
 app.use('/api-docs', swaggerDocs);
 
-// app.use(errorHandler);
+// global error handler
+app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-app.listen(port, () => {
-  console.log('Server listening on port ' + port);
-  console.log(`Swagger available at http://localhost:${port}/api-docs`);
-});
+app.listen(port, () => console.log('Server listening on port ' + port));
+console.log(`Swagger available at http://localhost:${port}/api-docs`);
